b48b7a15b4cd0662fbaea299ae7baf4b61018d43
jrhaning <43121518+jrhaning@users.noreply.github.com>
2018-12-03 03:06:07 -0800

    u

--- index.php
+++ index.php
@@ -3,4 +3,18 @@
     $lname = $_POST['lastname']
     $email = $_POST['email'];
     $message = $_POST['message'];
+    $from = 'From: Portfolio Site';
+    $to = 'jrhaning@lcmail.lcsc.edu';
+    $subject = 'Hello';
+
+    $body = "From: $fname .$lname\n E-Mail: $email\n Message:\n $message";
+    
+
+    if ($_POST['submit']) {
+        if (mail ($to, $subject, $body, $from)) { 
+            echo '<p>Your message has been sent!</p>';
+        } else { 
+            echo '<p>Something went wrong, go back and try again!</p>'; 
+        }
+    }
 ?>
